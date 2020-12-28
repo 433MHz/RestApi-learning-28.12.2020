0b1f3b0e2cd0a00a28d7eb6d67cda6e440305cea
Krystian <krystian.izdebski1999@gmail.com>
2020-12-28 23:58:58 +0100

    Working example of getting data from client and converting it from json to object inside server

--- src/main/java/pl/krystian/RestApi/EmployeeResource.java
+++ src/main/java/pl/krystian/RestApi/EmployeeResource.java
@@ -1,6 +1,12 @@
 package pl.krystian.RestApi;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -16,5 +22,15 @@ public class EmployeeResource {
 		ObjectToJSONConverter objectToJSONConverter = new ObjectToJSONConverter();
 		
 		return objectToJSONConverter.convertToJSON(employeeRepository.getEmployees());
+	}
+	
+	@POST
+	@Path("add")
+	public void addEmployee(String emp) {
+		Gson gson = new Gson();
+		Employee employee = gson.fromJson(emp, Employee.class);
+		
+		System.out.println("id: " + employee.getId() + " first name:" + employee.getfName() + " last name:" + employee.getlName() + " age:" + employee.getAge());
+	
 	}
 }
