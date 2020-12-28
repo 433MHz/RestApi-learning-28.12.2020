326959d8c83c06f62dd00535e9a4af2237262f3f
Krystian <krystian.izdebski1999@gmail.com>
2020-12-28 22:34:57 +0100

    Working example of parsing object to json format using Gson and showing it on page

--- src/main/java/pl/krystian/RestApi/EmployeeResource.java
+++ src/main/java/pl/krystian/RestApi/EmployeeResource.java
@@ -1,22 +1,34 @@
 package pl.krystian.RestApi;
 
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("employee")
 public class EmployeeResource {
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public Employee getEmployee() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getEmployee() {
 		
-		Employee emp = new Employee();
-		emp.setfName("Krystian");
-		emp.setlName("Izdebski");
-		emp.setAge(21);
+		Employee emp;
+		ArrayList<Employee> emps = new ArrayList<Employee>();
 		
-		return emp;
+		for (int i = 0; i <=1000; i ++) {
+			emp = new Employee();
+			emp.setfName("Krystian" + i);
+			emp.setlName("Izdebski" + i);
+			emp.setAge(i);
+			emps.add(emp);
+		}
+		
+		Gson gson = new Gson();
+		return gson.toJson(emps);
 	}
 }
